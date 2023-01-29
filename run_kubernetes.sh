1e3fe9708eb5fa30522e4d3f8604e9df98a7bc16
abdou <abdou@someone.com>
2023-01-29 18:41:16 +0100

    edit run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,16 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="mansouri96/sklearn:1.0"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run sklearn-demo\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=sklearn-demo
 
 # Step 3:
 # List kubernetes pods
-kubectl get pod
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward sklearn-demo 8000:80
